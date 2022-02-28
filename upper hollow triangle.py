d40f20aeb4633f6e1f417f60e73acb274860ba63
sugesh <96286770+SugeshG@users.noreply.github.com>
2022-02-28 11:44:03 +0530

    Add files via upload

---
+++ upper hollow triangle.py
@@ -0,0 +1,16 @@
+n=5
+for i in range(n):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for j in range(i):
+        if(j==0 or i==n-1 ):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ") 
+    for j in range(i+1):
+        if(j==i or i==n-1):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()     
+print() 
