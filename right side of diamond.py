17fe5ccd466fdfe581637fbc848867ca9c6923b1
sugesh <96286770+SugeshG@users.noreply.github.com>
2022-02-28 11:50:33 +0530

    Add files via upload

---
+++ right side of diamond.py
@@ -0,0 +1,10 @@
+#right side of diamond
+n=5
+for i in range(n-1):
+    for j in range(i+1):
+        print("*",end=" ")
+    print() 
+for i in range(n):
+    for j in range(n-i):
+        print("*",end=" ")
+    print() 
